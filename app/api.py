6bae403f8ff0cfc63f4372f8e004fe380343bfbc
Alexander Lim <alexsclim@gmail.com>
2018-01-13 20:36:31 -0800

    test

--- app/api.py
+++ app/api.py
@@ -1,6 +1,15 @@
 from app import app
+import ita
 
 @app.route('/')
 @app.route('/index')
 def index():
+    client = ita.Account("dragonboatfelist@gmail.com", "felist123")
+    client.trade("GOOG", ita.Action.buy, 10)
+    open_trades = client.get_open_trades()
+    for open_trade in open_trades:
+      print(open_trade.date_time)
+      print(open_trade.description)
+      print(open_trade.symbol)
+      print(open_trade.quantity)
     return "Hello, World!"
